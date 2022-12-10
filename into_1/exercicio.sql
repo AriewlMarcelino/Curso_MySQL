c4379e1130047b57e6b9eb486661ebd0cb162eb6
AriewlMarcelino <paulo.estudante9904@gmail.com>
2022-12-10 13:38:14 -0300

    aprendendo alguns comandos de introdução em SQL

---
+++ into_1/exercicio.sql
@@ -0,0 +1,11 @@
+/* USE<nomeDoBanco>Serve para podermos estar selecioando um banco.  */
+ /*USE empresa;*/
+ /*SELECT*FROM <nomeTabela>. Usamos para poder estar selecioando um tabela dentro do banco de dados*/
+ /*SELECT * FROM funcionarios;*/
+ 
+ 
+ /*Exercicio 2 -
+	Acesse a o banco com o USE e selecione a tabela com, o nome servição com a propriedade SELECT*FROM
+ */
+ USE empresa;
+ SELECT * FROM servicos;
